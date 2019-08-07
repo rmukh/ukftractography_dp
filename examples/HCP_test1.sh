229c151b1a95bab02cee85f145b2284c75fdfdaa
Rinat Mukhometzianov <mukhometzyanov@mail.ru>
2019-08-07 11:15:08 -0400

    some changes

--- examples/HCP_test1.sh
+++ examples/HCP_test1.sh
@@ -9,17 +9,17 @@ logFile="log.txt"
 BINARY='../build/UKFTractography-build/UKFTractography/bin/UKFTractography'
 
 # VOLUME
-dwi_path="/home/rinat/Desktop/HCP_subj/100307-dwi.nhdr"
+dwi_path="/home/rinat/Desktop/HCP_subj/100307-dwi-new-convert.nhdr"
 
 # MASK
-mask_path="/home/rinat/Desktop/HCP_subj/nodif_brain_mask.nrrd"
+mask_path="/home/rinat/Desktop/HCP_subj/100307_diff_mask.nrrd"
 
 # OUTPUT FIBER
-output_path="/home/rinat/Desktop/ukftests/HCP-test-no-FW.vtk"
+output_path="/home/rinat/Desktop/ukftests/HCP/HCP_odf_3_rtop1_600_qm_0.001.vtk"
 
 #csf_path="/data/pnl/home/rz892/HCP/spm/csf_mask.nrrd"
 #wm_path="/data/pnl/home/rz892/HCP/spm/wm_mask.nrrd"
-seeds_path="/home/rinat/Desktop/HCP_subj/seeds_spinal.nrrd"
+seeds_path="/home/rinat/Desktop/HCP_subj/seeds_stem.nrrd"
 echo "Start time $(date)" | tee $logFile
 
 # --seedsFile $seeds_path \
@@ -30,9 +30,10 @@ eval $BINARY \
  --seedsFile $seeds_path \
  --seedsPerVoxel 1 \
  --diffusionPropagator \
- --Qm 0.005 \
+ --minRTOP1stop 600 \
+ --Qm 0.001 \
  --stepLength 0.5 \
- --maxODFthresh 0.7 \
+ --maxODFthresh 0.3 \
  --recordNMSE \
  --recordWeights \
  --recordRTOP \
